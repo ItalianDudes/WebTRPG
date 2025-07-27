352ddd8013dc056b33d210413b3fbfe0c2463d01
Hacker6329 <Hacker6329@gmail.com>
2025-07-27 04:26:40 +0200

    Added testing purpose no-login paths

--- core/src/main/java/it/italiandudes/webtrpg/core/security/configuration/SecurityConfig.java
+++ core/src/main/java/it/italiandudes/webtrpg/core/security/configuration/SecurityConfig.java
@@ -23,7 +23,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(@NotNull final HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login","/register", "/web/css/**", "/favicon.ico", "/dnd5e/css_test", "/fallout/css_test").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/register",
+                                "/web/css/**",
+                                "/favicon.ico",
+                                "/**/*.css", // Da rimuovere prima della release
+                                "/**/css_test" // Da rimuovere prima della release
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
