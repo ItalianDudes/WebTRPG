a6c6076bfb9d96e46720c75a0870046f3fd5ee55
Hacker6329 <Hacker6329@gmail.com>
2025-08-16 04:37:32 +0200

    Adjusted parameters in DND5ESheetRepository according to column names

--- dnd5e/src/main/java/it/italiandudes/webtrpg/dnd5e/repository/DND5ESheetRepository.java
+++ dnd5e/src/main/java/it/italiandudes/webtrpg/dnd5e/repository/DND5ESheetRepository.java
@@ -18,7 +18,7 @@ public interface DND5ESheetRepository extends JpaRepository<DND5ESheet, Long> {
 
     // Find All By
     @NotNull List<@NotNull DND5ESheet> findAllByCampaignAndOwner(@NotNull DND5ECampaign campaign, @NotNull User owner);
-    @NotNull List<@NotNull DND5ESheet> findAllByOwnerAndCharacterName(User owner, String characterName);
+    @NotNull List<@NotNull DND5ESheet> findAllByOwnerAndTabCharacter_CharacterName(@NotNull User owner, @NotNull String characterName);
     @NotNull List<@NotNull DND5ESheet> findAllByCampaign(@NotNull DND5ECampaign campaign);
     @NotNull List<@NotNull DND5ESheet> findAllByOwner(@NotNull User owner);
 }
