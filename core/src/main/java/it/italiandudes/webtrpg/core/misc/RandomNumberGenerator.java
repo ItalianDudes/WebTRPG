9e4c59dc22ef21b52da2fb4c547387b23e46c62d
Hacker6329 <Hacker6329@gmail.com>
2025-08-19 18:07:38 +0200

    Implemented random number between included numbers

--- core/src/main/java/it/italiandudes/webtrpg/core/misc/RandomNumberGenerator.java
+++ core/src/main/java/it/italiandudes/webtrpg/core/misc/RandomNumberGenerator.java
@@ -9,14 +9,22 @@ public final class RandomNumberGenerator {
     public static final Random RANDOM = new Random();
 
     // Methods
-
     /**
-     * Randomize a number between two values
+     * Randomize a number between two values, min included, max excluded
      * @param min included in range
      * @param max excluded from range
      * @return random integer in form of [min, max[
      */
     public static int randomBetween(int min, int max) {
         return RANDOM.nextInt(max - min) + min;
+    }
+    /**
+     * Randomize a number between two values, all bounds included
+     * @param min included in range
+     * @param max included in range
+     * @return random integer in form of [min, max]
+     */
+    public static int randomBetweenBoundsIncluded(int min, int max) {
+        return RANDOM.nextInt((max + 1) - min) + min;
     }
 }
